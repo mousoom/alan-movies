b8beec55da8ecff645a9c1c76cb57f371227f2cd
Mousoom <mousoom44@gmail.com>
2021-08-31 20:09:29 +0530

    changed -webkit

--- src/component/MoviesCards.js
+++ src/component/MoviesCards.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import Card from "./card/card";
 import { Grid, Grow } from "@material-ui/core";
-import useStyles from "./style.js";
 import Alan from "../assest/alan-logo.png";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
@@ -15,13 +14,13 @@ import InfoIcon from "@material-ui/icons/InfoOutlined";
 import Tooltip from "@material-ui/core/Tooltip";
 import Chip from "@material-ui/core/Chip";
 import Avatar from "@material-ui/core/Avatar";
+import "./style.css";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 const MoviesCards = ({ list, activeMovies }) => {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [width, setWidth] = React.useState(window.innerWidth);
   const [size, setSize] = React.useState("");
@@ -55,14 +54,14 @@ const MoviesCards = ({ list, activeMovies }) => {
   console.log(view);
   function handleResize() {
     if (view === "mobile") {
-      setSize('4rem');
-      setSize1('1rem');
+      setSize("4rem");
+      setSize1("1rem");
     } else if (view === "tablet") {
-      setSize('5rem');
-      setSize1('1.5rem');
-    }else if (view === "laptop") {
-      setSize('8rem');
-      setSize1('2rem');
+      setSize("5rem");
+      setSize1("1.5rem");
+    } else if (view === "laptop") {
+      setSize("8rem");
+      setSize1("2rem");
     }
   }
   useEffect(() => {
@@ -77,7 +76,7 @@ const MoviesCards = ({ list, activeMovies }) => {
 
   if (!list.length) {
     return (
-      <div className={classes.main}>
+      <div className="main">
         <Dialog
           open={open}
           TransitionComponent={Transition}
@@ -173,7 +172,7 @@ const MoviesCards = ({ list, activeMovies }) => {
             </DialogContentText>
           </DialogContent>
         </Dialog>
-        <div className={classes.content}>
+        <div className="content">
           <div
             style={{
               display: "flex",
@@ -190,43 +189,39 @@ const MoviesCards = ({ list, activeMovies }) => {
           </div>
 
           <div>
-            {size && 
+            {size && (
+              <span
+                className="discover"
+                style={{
+                  display: "inline",
+                  fontWeight: "500",
+                  fontSize: size,
+                  letterSpacing: "3px",
+                }}
+              >
+                Discover.
+              </span>
+            )}
+          </div>
+          {size1 && (
             <span
-              className={classes.discover}
               style={{
-                display: "inline",
-                fontWeight: "500",
-                fontSize: size,
-                color: "transparent",
-                letterSpacing: "3px",
-                backgroundImage:
-                  "linear-gradient(150deg,#7a28ff 30%, #3d7aff 70%)",
-                backgroundClip: "text",
+                color: "white",
+                fontSize: size1,
+                fontWeight: "400",
+                letterSpacing: "10px",
+                marginTop: "-30px",
+                paddingLeft: "10px",
               }}
             >
-              Discover.
+              The Best Movies
             </span>
-  }
-          </div>
-          {size1 && 
-          <span
-            style={{
-              color: "white",
-              fontSize: size1,
-              fontWeight: "400",
-              letterSpacing: "10px",
-              marginTop: "-30px",
-              paddingLeft: "10px",
-            }}
-          >
-            The Best Movies
-          </span>
-  }
-          <div className={classes.brands}>
-            <div className={classes.brands1}>
+          )}
+          <div className="brands">
+            <div className="brands1">
               <img src={Alan} width="70px" alt="" />
             </div>
-            <div className={classes.brands1} style={{ marginLeft: "25px" }}>
+            <div className="brands1" style={{ marginLeft: "25px" }}>
               <img
                 src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
                 width="60px"
@@ -236,12 +231,12 @@ const MoviesCards = ({ list, activeMovies }) => {
           </div>
         </div>
         {/* <Tilt
-          className={classes.tilt}
+          className={tilt}
           options={{ max: 25 }}
           style={{ height: 350, width: 300 }}
         >
-          <div className={classes.Inner}>
-            <h1 className={classes.title}>ALAN-MOVIE</h1>
+          <div className={Inner}>
+            <h1 className={title}>ALAN-MOVIE</h1>
             <h2>Try a Command:</h2>
             <h3>"What are the best movies today?"</h3>
             <h2>Or</h2>
@@ -254,12 +249,7 @@ const MoviesCards = ({ list, activeMovies }) => {
 
   return (
     <Grow in>
-      <Grid
-        className={classes.container}
-        container
-        alignItems="stretch"
-        spacing={3}
-      >
+      <Grid className="container" container alignItems="stretch" spacing={3}>
         {list.map((list, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
             <Card list={list} i={i} activeMovies={activeMovies} />
