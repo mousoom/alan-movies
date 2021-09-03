2d7e23e6e55934283494dfcfc3d82a26fca7d9bf
Mousoom <mousoom44@gmail.com>
2021-09-03 14:29:49 +0530

    modified GA

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import ReactGA from 'react-ga';
 
 
 
-ReactGA.initialize(process.env.REACT_APP_GA);
+ReactGA.initialize('UA-206713505-1');
 
 const trackPage = page => {
   ReactGA.set({ page });
   ReactGA.pageview(page);
 };
 
-const alanKey = process.env.REACT_APP_ALAN_KEY;
+const alanKey =
+  "5c0dde699f7be267f2b24fa877066ad02e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const COMMANDS = {
   SHOW_MOVIE: "showMovie",
